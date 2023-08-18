93d6f5589839da0ecff785ba332658482c2b24ff
BeytullahYayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-08-18 10:34:07 -0400

    Triangle.msg and script file updated

--- scripts/triangle_move.py
+++ scripts/triangle_move.py
@@ -3,20 +3,21 @@
 import rospy
 from aisec.msg import Triangle
 
-def moveTurtle():
+
+
+def triangle_move():
     rospy.init_node("triangle_move")
     pub=rospy.Publisher("cmd_vel",Triangle)
-    speed_message=Triangle()
-    speed_message.linear_x=0.5
+    speed_message=Triangle()#Type of speed message
+    speed_message.Linear.x=0.5#Speed of the robot
     distance=5
-    displacement=0
-    t0=rospy.Time.now().to_sec()#Start time
+    displacement=0#Displacement value of the robot
+    t0=rospy.Time.now().to_sec()#Starting time as second types
     while displacement<distance:
         pub.publish(speed_message)
-        t1=rospy.Time.now().to_sec()
+        t1=rospy.Time.now().to_sec()#Current time to check robot's displacement value
         displacement=(t1-t0)*speed_message.linear_x
-    
-    speed_message.linear_x=0
+    speed_message.Linear.x=0
     pub.publish(speed_message)
     
-moveTurtle()
+triangle_move()
