d63f9515d74f049e3c184f9a8480d914e16c93ff
BeytullahYayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-08-18 09:54:41 -0400

    message file created

--- scripts/triangle_move.py
+++ scripts/triangle_move.py
@@ -1,3 +1,22 @@
 #! /usr/bin/env python3
 
 import rospy
+from aisec.msg import Triangle
+
+def moveTurtle():
+    rospy.init_node("triangle_move")
+    pub=rospy.Publisher("cmd_vel",Triangle)
+    speed_message=Triangle()
+    speed_message.linear_x=0.5
+    distance=5
+    displacement=0
+    t0=rospy.Time.now().to_sec()#Start time
+    while displacement<distance:
+        pub.publish(speed_message)
+        t1=rospy.Time.now().to_sec()
+        displacement=(t1-t0)*speed_message.linear_x
+    
+    speed_message.linear_x=0
+    pub.publish(speed_message)
+    
+moveTurtle()
