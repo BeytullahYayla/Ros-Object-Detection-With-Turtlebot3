6f18142e89c62df9ea85d030ae06a2710dc8ec8b
BeytullahYayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-08-20 09:10:33 -0400

    triangle script updated

--- scripts/triangle_move.py
+++ scripts/triangle_move.py
@@ -1,74 +1,58 @@
 #!/usr/bin/env python3
 import rospy
-import math
 from geometry_msgs.msg import Twist
-from aisec.msg import Triangle
-def move(distance):
-    velocity_publisher = rospy.Publisher('/cmd_vel', Triangle, queue_size=10)
-    vel_msg = Triangle()
+import math
 
-    # İleri git
-    vel_msg.linear.x = 0.2  # Örneğin, 0.2 m/s hızında ileri git
-    vel_msg.linear.y = 0
-    vel_msg.linear.z = 0
-
-    t0 = rospy.Time.now().to_sec()
+def move_distance(pub, linear_speed, distance):
+    twist = Twist()
+    twist.linear.x = linear_speed
     current_distance = 0
+    rate = rospy.Rate(10)  # 10 Hz
+    t0=rospy.Time.now().to_sec()
 
-    while current_distance < distance:
-        velocity_publisher.publish(vel_msg)
-        t1 = rospy.Time.now().to_sec()
-        current_distance = 0.2 * (t1 - t0)
+    while current_distance < distance and not rospy.is_shutdown():
+        pub.publish(twist)
+        t1=rospy.Time.now().to_sec()
+        rate.sleep()
+        current_distance += linear_speed * 0.1  # Distance = Speed * Time
 
-    # Dur
-    vel_msg.linear.x = 0
-    velocity_publisher.publish(vel_msg)
+    # Stop the robot
+    twist.linear.x = 0
+    pub.publish(twist)
 
-def turn_robot(angle,direction):
-   
-    velocity_publisher = rospy.Publisher('/cmd_vel', Triangle, queue_size=10)
-    vel_msg = Triangle()
-
-    # Dön
-    if direction=="positive":
-        
-        vel_msg.angular.z = 0.5  # Örneğin, 0.5 rad/s hızında dön
-    elif direction=="negative":
-        vel_msg.angular.z=-0.5
-        
-    t0 = rospy.Time.now().to_sec()
+def rotate(pub, angular_speed, angle):
+    twist = Twist()
+    twist.angular.z = angular_speed
     current_angle = 0
+    rate = rospy.Rate(10)  # 10 Hz
 
-    while current_angle < angle:
-        velocity_publisher.publish(vel_msg)
-        t1 = rospy.Time.now().to_sec()
-        current_angle = 0.5 * (t1 - t0)
+    while current_angle < angle and not rospy.is_shutdown():
+        pub.publish(twist)
+        rate.sleep()
+        current_angle += angular_speed * 0.1  # Angle = Speed * Time
 
-    # Dur
-    vel_msg.angular.z= 0
-    velocity_publisher.publish(vel_msg)
+    # Stop the robot
+    twist.angular.z = 0
+    pub.publish(twist)
 
-rospy.init_node('robot_movement_node', anonymous=True)
+def draw_equilateral_triangle(pub, side_length):
+    linear_speed = 0.25  # Adjust as needed
+    angular_speed = 0.7854  # Adjust as needed
+
+    for _ in range(3):
+        move_distance(pub, linear_speed, side_length)
+        rotate(pub, angular_speed, math.radians(120))
+
+def main():
+    rospy.init_node('equilateral_triangle_node', anonymous=True)
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+    side_length = 1.04  # Adjust as needed
+
+    draw_equilateral_triangle(pub, side_length)
 
 
 try:
-    
-        
-        
-        move(1)  # 3 metre ileri git
-        rospy.sleep(1)  # 1 saniye bekle
-        turn_robot(math.radians(60),"negative")  # 120 derece dön (radyan cinsinden)
-        rospy.sleep(1)  # 1 saniye bekle
-        move(1)
-        turn_robot(math.radians(120),"positive")  
-        rospy.sleep(1)  # 1 saniye bekle
-        move(1)
-        rospy.sleep(1)  # 1 saniye bekle
-        turn_robot(math.radians(120),"positive")
-        rospy.sleep(1)  # 1 saniye bekle
-        move(1)     
-        rospy.sleep(1) 
-        
-        
+        main()
 except rospy.ROSInterruptException:
-        print("Calisma Durdu")
+        pass
